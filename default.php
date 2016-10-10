e8bc5c19f30478d7a9c5eafd04aea6ff43fff35e
Ju Hong Kim <nabimoon1234@gmail.com>
2016-10-09 23:17:39 -0400

    Update default.php

--- default.php
+++ default.php
@@ -1,5 +1,5 @@
 <?php
-$file = "/2015/anime/anime-2015.css";
+$file = "2015/anime/anime-2015.css";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
