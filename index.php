f3c3c93b850d51afe76bef5dabe52110fb4f9865
Ju Hong Kim <nabimoon1234@gmail.com>
2016-10-25 12:28:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
+<html>
+  <head>
+    <title>Hayao Miyazaki Tribute</title>
+     <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class = "fluid-container">
+      
+    <h1 class = 'primary-text text-center page-header header'>MY PERSONAL MAL CSS STYLE</h1>
+    <h3 class = 'text-center sub-header'>MyanimeList Styles</h3>
+    
+    
+    </div>
+    
+  </body>
+</html>
